025a10f986be6debb7568440d0a1ca40cdfdc1a4
Lucas Melo <luluco250@gmail.com>
2018-08-29 12:05:53 -0300

    Added a configuration file, optimized a few things and added an optional, slightly more complex algorithm that avoids the bug where the character will first start the sprint animation before reverting to jogging; it's enabled by default, but can be reverted to the simpler algorithm via a configuration.

--- ModScript.cs
+++ ModScript.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using GTA;
 using GTA.Native;
@@ -7,31 +7,69 @@ using GTA.Native;
 public class ModScript : Script {
 	int tickInterval;
 	int sprintInterval;
-	bool useCapsLock;
 
-	Stopwatch stopwatch = new Stopwatch();
+	int lastClickTime = Game.GameTime;
+	bool isSprinting = false;
 
 	public ModScript() {
-		tickInterval = Settings.GetValueInteger("tickInterval", "Settings", 100);
-		sprintInterval = Settings.GetValueInteger("sprintInterval", "Settings", 300);
-		useCapsLock = Settings.GetValueBool("useCapsLock", "Settings", true);
+		tickInterval = Settings.GetValueInteger("TickInterval", "Settings", 100);
+		sprintInterval = Settings.GetValueInteger("SprintInterval", "Settings", 300);
 
-		stopwatch.Start();
 		KeyDown += new GTA.KeyEventHandler(OnKeyDown);
+
+		bool useCapsLock = Settings.GetValueBool("UseCapsLock", "Settings", true);
 		Interval = tickInterval;
-		Tick += new EventHandler(OnTick);
+
+		if (Settings.GetValueBool("UseSimpleAlgorithm", "Settings", false)) {
+			if (useCapsLock)
+				Tick += new EventHandler(OnTick_Simple);
+			else
+				Tick += new EventHandler(OnTick_SimpleNoCapsLock);
+		} else {
+			if (useCapsLock)
+				Tick += new EventHandler(OnTick);
+			else
+				Tick += new EventHandler(OnTick_NoCapsLock);
+		}
 	}
 
 	void OnTick(object sender, EventArgs e) {
-		//	Can sprint if caps is activated or
-		//	repeatedly pressing left shift.
-		CanSprint((useCapsLock && Control.IsKeyLocked(Keys.CapsLock))
-		|| stopwatch.ElapsedMilliseconds < sprintInterval);
+		if (Control.IsKeyLocked(Keys.CapsLock))
+			return;
+		
+		OnTick_NoCapsLock(sender, e);
+	}
+
+	void OnTick_NoCapsLock(object sender, EventArgs e) {
+		if ((Game.GameTime - lastClickTime) < sprintInterval) {
+			// Don't sprint immediately
+			if (isSprinting)
+				CanSprint(true);
+			
+			isSprinting = true;
+		} else {
+			if (isSprinting)
+				CanSprint(false);
+			
+			isSprinting = false;
+		}
+	}
+
+	void OnTick_Simple(object sender, EventArgs e) {
+		DoSimple(Control.IsKeyLocked(Keys.CapsLock));
+	}
+
+	void OnTick_SimpleNoCapsLock(object sender, EventArgs e) {
+		DoSimple();
+	}
+
+	void DoSimple(bool caps = false) {
+		CanSprint(caps || (Game.GameTime - lastClickTime) < sprintInterval);
 	}
 
 	void OnKeyDown(object sender, GTA.KeyEventArgs e) {
 		if (e.Key == Keys.LShiftKey)
-			stopwatch.Restart();
+			lastClickTime = Game.GameTime;
 	}
 
 	void CanSprint(bool b) {
